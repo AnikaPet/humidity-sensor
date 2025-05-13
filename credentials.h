39700693cd24b00440b53064d280c3fab99220cd
Anika Petrovic <anika.petrovic1999@gmail.com>
2025-05-13 14:47:50 +0200

    Put credentials in separate file.

---
+++ credentials.h
@@ -0,0 +1,4 @@
+#define ssid myssidwhatever
+#define pass mypassword
+#define device_name devicename
+#define access_token accesstoken
--- humidity-sensor.ino
+++ humidity-sensor.ino
@@ -1,16 +1,14 @@
-#include <PubSubClient.h>  // include library we included
-#include <ESP8266WiFi.h>
+#include <PubSubClient.h> 
 #include <stdlib.h>
 
 #include <DHT.h>
 
-#define DPIN 4
+#include <credentials.h>
+
+#define DPIN 4 // GPIO2
 #define DTYPE DHT11
 
 DHT dht(DPIN,DTYPE);
-
-const char *ssid = "***";  // replace with your wifi ssid and wpa2 key
-const char *pass = "***";  //WiFi Password
 
 WiFiClient client;
 PubSubClient pubsub_client(client);
@@ -34,7 +32,7 @@ void setup() {
 
   pubsub_client.setServer("demo.thingsboard.io", 1883);
 
-  while (!pubsub_client.connect("***", "***", NULL)) {
+  while (!pubsub_client.connect(device_name, access_token, NULL)) {
     delay(500);
   Serial.print(".");
 }
@@ -47,19 +45,14 @@ void loop() {
   delay(2000);
 
   float humidity = dht.readHumidity();
-  float temperature = dht.readTemperature();
-
   Serial.println(humidity);
-  Serial.println("temperature:");
-  Serial.println(temperature);
-  /*
+
   String payload = "{";
-  payload += "\"rssi\":"; payload += strptr;
+  payload += "\"humidity\":"; payload += strptr;
   payload += "}";
   Serial.println(payload);
 
   if(pubsub_client.publish("v1/devices/me/telemetry",payload.c_str()))
     Serial.println("Published");
 
-  */
 }
